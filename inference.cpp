d1d4d2dc0768cdd42da145f18017d187b40b0b59
borisnorm <borisnorm@gmail.com>
2026-06-02 16:55:34 -0700

    1. modify infernece.cpp 2.check if vscode was added

--- inference.cpp
+++ inference.cpp
@@ -1,8 +1,15 @@
+#include <iostream>
+#include <vector>
 
+using namespace std;
 
-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+void main() {
+    vector<int> numbers = {1, 2, 3, 4, 5};
+    int sum = 0;
 
+    for (int num : numbers) {
+        sum += num;
+    }
 
-
-
+    cout << "The sum of the numbers is: " << sum << endl;
+}
